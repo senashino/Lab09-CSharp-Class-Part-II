fecb2a4edc47f0100bb26681e5760b806611c27b
senashino <phwn26@gmail.com>
2025-02-08 17:35:56 +0700

    update lab

---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿Person.name = "Kannika";
+Console.WriteLine(Person.GetName());
+var student = new Student();
+student.name = "Phugun";
+Console.WriteLine(Person.GetName());
+Console.WriteLine($"Student name: {student.name}");
+static class Person
+{
+    static public string? name;
+    static public string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student 
+{
+    public string? name;
+}
