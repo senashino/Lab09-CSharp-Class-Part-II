127662c91873955e6081011bb5da36013e067f3d
senashino <phwn26@gmail.com>
2025-02-08 21:10:48 +0700

    update

--- Lab09_Ex01/obj/Debug/net9.0/Lab09_Ex01.AssemblyInfo.cs
+++ Lab09_Ex01/obj/Debug/net9.0/Lab09_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab09_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76cf0eb8779ba72f2ffbf5c3259504500c3131ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecb2a4edc47f0100bb26681e5760b806611c27b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab09_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab09_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
