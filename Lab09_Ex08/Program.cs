132a42f0f07dbd1275967d34eea9218ea9056e40
senashino <phwn26@gmail.com>
2025-02-09 14:07:35 +0700

    Update Lab09

---
+++ Lab09_Ex08/Program.cs
@@ -0,0 +1,11 @@
+﻿Circle.PrintCircleArea(100);
+
+class Circle
+{
+    public static void PrintCircleArea(double radius)
+    {
+        double PI = 3.14159;
+        PI = 3.14159265359;
+        System.Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+    }
+}
